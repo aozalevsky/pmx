f039d9833e0e2d50333ec3d282bcfe542b83f568
Arthur Zalevsky <aozalevsky@fbb.msu.ru>
2016-02-05 17:42:26 +0300

    from distutils to setuptools

--- setup.py
+++ setup.py
@@ -28,62 +28,50 @@
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # ----------------------------------------------------------------------
 
-import os,sys
-from distutils.core import setup, Extension
-from distutils.command.install_data import install_data
+from os import path
+# Always prefer setuptools over distutils
+from setuptools import setup
+# For extension managements
+from setuptools.extension import Extension
+from Cython.Build import cythonize
 
-class install_data_files(install_data):
-
-    def run(self):
-        install_cmd = self.get_finalized_command('install')
-        self.install_dir = getattr(install_cmd, 'install_lib')
-        return install_data.run(self)
-
+here = path.abspath(path.dirname(__file__))
 
 pmx = Extension('pmx/_pmx',
-                   libraries = ['m'],
-                   include_dirs = ['src/pmx'],
-                   sources = ['src/pmx/Geometry.c','src/pmx/wrap_Geometry.c',
-                              'src/pmx/init.c', 'src/pmx/Energy.c']
-                   
-                 )
+                libraries=['m'],
+                include_dirs=['src/pmx'],
+                sources=['src/pmx/Geometry.c', 'src/pmx/wrap_Geometry.c',
+                         'src/pmx/init.c', 'src/pmx/Energy.c']
+                )
 
 xdrio = Extension('pmx/_xdrio',
-                   libraries = ['m'],
-                   include_dirs = ['src/xdr'],
-                   sources = ['src/xdr/xdrfile.c','src/xdr/xdrfile_trr.c',
-                              'src/xdr/xdrfile_xtc.c']
-                   
-                 )
+                  libraries=['m'],
+                  include_dirs=['src/xdr'],
+                  sources=['src/xdr/xdrfile.c', 'src/xdr/xdrfile_trr.c',
+                           'src/xdr/xdrfile_xtc.c']
+                  )
 
 ## cpp_test = Extension('pmx/_cpp_test',
 ##                    libraries = ['m'],
 ##                    include_dirs = ['src'],
 ##                    sources = ['src/cpp_test.cpp']
-                   
+
 ##                  )
+extenstions = [pmx, xdrio]
 
+with open('README.rst', 'r') as f:
+    long_description = f.read()
 
-
-setup (name = 'pmx',
-       version = '1.1.0dev',
-       description = 'Python Toolbox structure file editing and writing simulation setup/analysis tools',
-       author = 'Daniel Seeliger',
-       author_email = 'seeliger.biosoft@gmail.de',
-       url = 'https://github.com/dseeliger/pmx/',
-       long_description = '''Tools to play with structure files, topologies, index files, xtc files, etc....''',
-       packages = ['pmx'],
-       data_files = [('pmx/data',['data/bbdep.pkl']),
-                     ('pmx/data',['data/bp.pkl']),
-                     #('pmx/data',['data/fragments.pkl']),
-                     ('pmx/data',['data/ffamber99sb.rtp']),
-                     ('pmx/data',['data/ffamber99sbbon.itp']),
-                     ('pmx/data',['data/ffamber99sbnb.itp']),
-                     ('pmx/data',['data/blosum62_new.mat'])
-                     ],
-       ext_modules = [pmx,xdrio],
-       cmdclass = {'install_data': install_data_files
-                   },
-
-       )
-
+setup(
+    name='pmx',
+    version='1.1.1dev',
+    description='Python Toolbox structure file editing and \
+        writing simulation setup/analysis tools',
+    author='Arthur Zalevsky <aozalevsky.fbb.msu.ru>, \
+        Daniel Seeliger <seeliger.biosoft@gmail.de>',
+    author_email='aozalevsky@fbb.msu.ru',
+    url='https://github.com/dseeliger/pmx/',
+    long_description=long_description,
+    packages=['pmx'],
+    ext_modules=cythonize(extenstions)
+)
