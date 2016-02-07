0ff7f3648079a049aaafebae42a600633d5452b5
Arthur Zalevsky <aozalevsky@fbb.msu.ru>
2016-02-07 19:25:10 +0300

    Cleaned according pep8, added filename attribute

--- pmx/ndx.py
+++ pmx/ndx.py
@@ -27,81 +27,91 @@
 # CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # ----------------------------------------------------------------------
-from parser import *
+
+from parser import readSection
 import re
-import sys    
-#---------------------------------------------------
+import sys
+
+# ---------------------------------------------------
+
+
 class IndexGroup:
     """ CLASS TO DEAL WITH GROMACS INDEX FILES"""
-    def __init__(self, name = '', ids = [], atoms = []):
+
+    def __init__(self, name='', ids=[], atoms=[]):
         self.ids = []
         if atoms:
-            self.ids = map(lambda a: a.id, atoms )
+            self.ids = map(lambda a: a.id, atoms)
         else:
             self.ids = ids
         self.name = name
         self.ids.sort()
-        
+
     def __str__(self):
         s = ''
-        s+= '[ %s ]\n' % self.name
+        s += '[ %s ]\n' % self.name
         count = 0
         for i in range(len(self.ids)):
             if len(str(self.ids[i])) > 6:
                 fs = "%d "
             else:
                 fs = "%6d "
-            s+=fs % self.ids[i]
-            count+=1
-            if count % 15 == 0: s+='\n'
+            s += fs % self.ids[i]
+            count += 1
+            if count % 15 == 0:
+                s += '\n'
         return s
 
-    def read_index_group(self,name,lines):
+    def read_index_group(self, name, lines):
         if '[' in name:
             name = name[1:-1].strip()
         self.name = name
-        key = '[ '+name+' ]'
-        ll = readSection(lines,key,'[')
+        key = '[ ' + name + ' ]'
+        ll = readSection(lines, key, '[')
         self.ids = []
         for line in ll:
             self.ids.extend([int(x) for x in line.split()])
         return self
 
-    def select_atoms( self, atom_sel):
+    def select_atoms(self, atom_sel):
         atoms = []
         for idx in self.ids:
             for atom in atom_sel.atoms:
                 if atom.id == idx:
-                    atoms.append( atom )
+                    atoms.append(atom)
         return atoms
 
-#---------------------------------------------------
+# -------------------------------------------------
+
 
 class IndexFile:
 
-    def __init__(self, fname = None, names = [], groups = []):
-        self.groups = []
-        self.names = []
-        self.dic = {}
+    filename = None
+    groups = list()
+    names = list()
+    dic = dict()
+
+    def __init__(self, fname=None, names=[], groups=[]):
         if fname is not None:
+            self.filename = fname
             self.parse(fname)
         if groups:
             for g in groups:
-                self.add_group( g )
+                self.add_group(g)
 
-    def parse(self,fp):
-        if hasattr(fp,"read"):
+    def parse(self, fp):
+        if hasattr(fp, "read"):
             f = fp.read()
         else:
             f = open(fp).read()
         names = self.__get_index_groups(f)
         lines = f.split('\n')
         for name in names:
-            idx = IndexGroup().read_index_group(name,lines)
-            self.add_group( idx )
+            idx = IndexGroup().read_index_group(name, lines)
+            self.add_group(idx)
         return self
 
-    def __get_index_groups(self,f):
+    def __get_index_groups(self, f):
         x = re.compile('\[ .* \]\n')
         names = x.findall(f)
         r = []
@@ -109,53 +119,53 @@ class IndexFile:
             r.append(name.strip())
         return r
 
-    def write(self,fn=None,fp=None):
+    def write(self, fn=None, fp=None):
         if not fn and not fp:
             fp = sys.stdout
         if fn:
-            fp = open(fn,'w')
+            fp = open(fn, 'w')
         for gr in self.groups:
-            print >>fp, str(gr)+'\n'
-
+            print >>fp, str(gr) + '\n'
 
     def __str__(self):
         s = 'Gromacs index file (%d index groups):\n' % len(self.groups)
-        for i, group in enumerate( self.groups ):
-            s+=' %d  %-20s   :    %d atoms\n' % (i, group.name, len(group.ids) )
+        for i, group in enumerate(self.groups):
+            s += ' %d  %-20s   :    %d atoms\n' % (i,
+                                                   group.name, len(group.ids))
         return s
 
     def __getitem__(self, item):
         return self.dic[item]
 
-    def add_group( self, group ):
+    def add_group(self, group):
         if group.name in self.names:
             print >> sys.stderr, "IndexFile has group %s !! " % group.name
             print >> sys.stderr, "Group %s will be replaced !!" % group.name
-            self.delete_group( group.name )
-        self.names.append( group.name )
-        self.groups.append( group )
+            self.delete_group(group.name)
+        self.names.append(group.name)
+        self.groups.append(group)
         self.dic[group.name] = group
 
-    def delete_group( self, name ):
+    def delete_group(self, name):
         idx = -1
         for i, group in enumerate(self.groups):
             if group.name == name:
                 idx = i
                 break
-        if idx == -1: return
+        if idx == -1:
+            return
         del self.groups[idx]
         del self.dic[name]
-        self.names.remove( name )
+        self.names.remove(name)
 
-    def __delitem__(self, item ):
-        self.delete_group( item )
-    
-                           
-
-#---------------------------------------------------
+    def __delitem__(self, item):
+        self.delete_group(item)
 
 
-def get_index(atom_list = None, residue_list = None, chain_list = None):
+# --------------------------------------------------
+
+
+def get_index(atom_list=None, residue_list=None, chain_list=None):
     """ return atom indices from a list of atoms/residues/chains"""
     if not atom_list and not residue_list and not chain_list:
         print 'Error: Need list~'
@@ -166,18 +176,14 @@ def get_index(atom_list = None, residue_list = None, chain_list = None):
     if residue_list:
         al = []
         map(lambda r: al.extend(r.atoms), residue_list)
-        return get_index(atom_list = al)
+        return get_index(atom_list=al)
     if chain_list:
         al = []
         map(lambda c: al.extend(c.atoms), chain_list)
-        return get_index(atom_list = al)
-    
-        
+        return get_index(atom_list=al)
+
+
 def make_index_group(atomlist, name):
     lst = get_index(atomlist)
-    g = IndexGroup(ids = lst, name = name)
+    g = IndexGroup(ids=lst, name=name)
     return g
-
-    
-
-
