08270859b70c68fb5eb8db7276258c2ad68bf99d
Arthur Zalevsky <aozalevsky@fbb.msu.ru>
2016-03-30 19:59:14 +0300

    updated according to pep8

--- pmx/atom.py
+++ pmx/atom.py
@@ -27,7 +27,17 @@
 # CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # ----------------------------------------------------------------------
-__doc__="""
+
+import sys
+# from numpy import *
+import copy
+import library
+import _pmx as _p
+from math import pi
+from library import pdb_format, pdb_format2
+
+
+__doc__ = """
 This module contains the Atom class. It basically stores Atom
 properties but also has some methods for fast calculations.
 Basic Usage:
@@ -45,19 +55,18 @@ Basic Usage:
 
       - output
       >>> print atom   # prints atom in pdb format
-      
+
 
 """
-import _pmx as _p
-from numpy import *
-import copy, library
-from library import pdb_format, pdb_format2
+
 
 class Atom:
-    """ class for storage of atom properties and methods"""
-    def __init__(self, line = None,  mol2line = None, **kwargs):
 
-        self.race='ATOM  '
+    """ class for storage of atom properties and methods"""
+
+    def __init__(self, line=None,  mol2line=None, **kwargs):
+
+        self.race = 'ATOM  '
         self.id = 0
         self.orig_id = 0
         self.code = 0
@@ -65,7 +74,7 @@ class Atom:
         self.altloc = ''
         self.resname = ''
         self.chain_id = ' '
-        self.x = [0,0,0]
+        self.x = [0, 0, 0]
         self.occ = 1.
         self.bfac = 0.
         self.vdw = 0.
@@ -84,7 +93,7 @@ class Atom:
         self.chain = None
         self.model = None
         self.cgnr = 0.
-        self.v = [0,0,0]
+        self.v = [0, 0, 0]
         self.f = []
         self.m = 0.
         self.q = 0.
@@ -94,45 +103,45 @@ class Atom:
         self.typeB = ''
         self.resnr = 0
         self.grpnr = ' '
-        self.atomtype=''
-        self.atomtypeB=''
+        self.atomtype = ''
+        self.atomtypeB = ''
         self.ptype = ''
         self.long_name = ''
-        self.unity='A'
+        self.unity = 'A'
         for key, val in kwargs.items():
-            setattr(self,key,val)
+            setattr(self, key, val)
         if line is not None:
             self.readPDBString(line)
         if mol2line is not None:
             self.read_mol2_line(mol2line)
-            
-    def readPDBString(self,line):
+
+    def readPDBString(self, line):
         """PDB String to Atom"""
 
-        self.race=line[0:6]
-        self.id=int(line[7:11])
-        self.name=line[12:16].strip()
-        self.altloc=line[16]
-        self.resname=line[17:21].strip()
-        self.chain_id=line[21]
+        self.race = line[0:6]
+        self.id = int(line[7:11])
+        self.name = line[12:16].strip()
+        self.altloc = line[16]
+        self.resname = line[17:21].strip()
+        self.chain_id = line[21]
         try:
-            self.resnr=int(line[22:27])
+            self.resnr = int(line[22:27])
         except:
-            self.resnr=line[22:27] # contains insertion code
+            self.resnr = line[22:27]  # contains insertion code
 #            self.resnr=int(line[22:27][:-1])
-            
-        self.x=[float(line[30:38]),\
-                           float(line[39:46]),float(line[47:54])]
+
+        self.x = [float(line[30:38]),
+                  float(line[39:46]), float(line[47:54])]
         try:
-            self.occ=float(line[55:60])
+            self.occ = float(line[55:60])
         except:
             self.occ = 1.
         try:
-            self.bfac=float(line[61:66])
+            self.bfac = float(line[61:66])
         except:
             self.bfac = 0.
         try:
-            self.symbol=line[70:73].strip()
+            self.symbol = line[70:73].strip()
         except:
             self.symbol = None
         self.unity = 'A'
@@ -140,21 +149,20 @@ class Atom:
             self.get_symbol()
         return self
 
-    def dist(self,other):
+    def dist(self, other):
         """ returns the distance between two atoms
         Usage: dist=atom1.dist(atom2)
         This function is also called by typing
         d=atom1-atom2"""
-        return _p.dist(self.x,other.x)
+        return _p.dist(self.x, other.x)
 
-
-    def dist2(self,other):
+    def dist2(self, other):
         """ returns the squared distance between two atoms
         Usage: dist=atom1.dist2(atom2)"""
 
-        return _p.dist2(self.x,other.x)
+        return _p.dist2(self.x, other.x)
 
-    def __sub__(self,other):
+    def __sub__(self, other):
         """ Overloading of the '-' operator for using
         atom1-atom2 instead of atom1.dist(atom2)"""
         return self.dist(other)
@@ -166,123 +174,119 @@ class Atom:
 
     def nm2a(self):
         if self.unity == 'nm':
-            self.x[0]*=10
-            self.x[1]*=10
-            self.x[2]*=10
+            self.x[0] *= 10
+            self.x[1] *= 10
+            self.x[2] *= 10
             self.unity = 'A'
 
     def a2nm(self):
         if self.unity == 'A':
-            self.x[0]*=.1
-            self.x[1]*=.1
-            self.x[2]*=.1
+            self.x[0] *= .1
+            self.x[1] *= .1
+            self.x[2] *= .1
             self.unity = 'nm'
-            
-    def angle(self,other1,other2,degree=None):
+
+    def angle(self, other1, other2, degree=None):
         """ Calcluates the angle between 3 atoms
         Usage: atom1.angle(atom2,atom3)
         The degree flag causes the function to return the angle
         in degrees.
         (Note: atom1 must be between 2 and 3)"""
-        
-        angle= _p.angle(other1.x,self.x,other2.x)
+
+        angle = _p.angle(other1.x, self.x, other2.x)
         if degree:
-            return angle*180.0/pi
+            return angle * 180.0 / pi
         else:
             return angle
 
-    def dihedral(self,other1,other2,other3,degree=None):
-
+    def dihedral(self, other1, other2, other3, degree=None):
         """ Calculates the dihedral between four atoms.
         Usage: atom1.dihedral(atom2,atom3,atom4)
         The degree flag causes the function to return the dihedral
         in degrees."""
-        
-        ang=_p.dihedral(self.x,other1.x,other2.x,other3.x)
+
+        ang = _p.dihedral(self.x, other1.x, other2.x, other3.x)
         if degree:
-            return ang*180.0/pi
+            return ang * 180.0 / pi
         else:
             return ang
 
     def __str__(self):
         """ prints the atom in PDB format """
-        if self.unity=='nm':
-            coords = map(lambda x: x*10, self.x)
+        if self.unity == 'nm':
+            coords = map(lambda x: x * 10, self.x)
         else:
             coords = self.x
-        if len(self.resname)<4:
-            resname=self.resname+' '#[0:3]
+        if len(self.resname) < 4:
+            resname = self.resname + ' '  # [0:3]
         else:
-            resname=self.resname
-        if len(self.name)==1:
-            name=' '+self.name+'  '
-        elif len(self.name)==2:
+            resname = self.resname
+        if len(self.name) == 1:
+            name = ' ' + self.name + '  '
+        elif len(self.name) == 2:
             if self.name[0].isdigit():
-                name = self.name+'  '
+                name = self.name + '  '
             else:
-                name=' '+self.name+' '
-        elif len(self.name)==3:
+                name = ' ' + self.name + ' '
+        elif len(self.name) == 3:
             if self.name[0].isdigit():
-                name = self.name+' '
+                name = self.name + ' '
             else:
-                name=' '+self.name
+                name = ' ' + self.name
         else:
-            name=self.name
+            name = self.name
         idx = self.id % 100000
         try:
             resid = self.resnr % 10000
         except:
             resid = str(self.resnr)
         try:
-            s=pdb_format % (self.race,idx,name,self.altloc,\
-                             resname,self.chain_id,resid,\
-                             coords[0],coords[1],\
-                             coords[2],self.occ,self.bfac,self.symbol)
+            s = pdb_format % (self.race, idx, name, self.altloc,
+                              resname, self.chain_id, resid,
+                              coords[0], coords[1],
+                              coords[2], self.occ, self.bfac, self.symbol)
         except:
-            s=pdb_format2 % (self.race,idx,name,self.altloc,\
-                             resname,self.chain_id,resid,\
-                             coords[0],coords[1],\
-                             coords[2],self.occ,self.bfac)#,self.symbol)
+            s = pdb_format2 % (self.race, idx, name, self.altloc,
+                               resname, self.chain_id, resid,
+                               coords[0], coords[1],
+                               coords[2], self.occ, self.bfac)  # ,self.symbol)
         return s
 
-
-
-    def set_resname(self,resname):
+    def set_resname(self, resname):
 
         self.resname = resname
 
-    def set_chain_id(self,chain_id):
+    def set_chain_id(self, chain_id):
         """ change chain identifier"""
         self.chain_id = chain_id
-
 
     def make_long_name(self):
         """ make extended name to determine element
         and order"""
         # check for aliases first
         ali = library._aliases
-        if ali.has_key(self.resname) and \
-           ali[self.resname].has_key(self.name):
+        if self.resname in ali and \
+           self.name in ali[self.resname]:
             name = ali[self.resname][self.name]
         else:
             name = self.name.strip()
-        if len(name)==4:
+        if len(name) == 4:
             if not name[0].isdigit():
                 tmp = name[3]
-                name = tmp+name[:-1]
-        elif len(name)==3:
+                name = tmp + name[:-1]
+        elif len(name) == 3:
             if name[0].isdigit():
-                name+=' '
+                name += ' '
             else:
-                name=' '+name
-        elif len(name)==2:
-            if name[1].isdigit() and name[0]=='H' and \
-                   self.resname in library._aacids:
-                name = name[1]+name[0]+'  '
+                name = ' ' + name
+        elif len(name) == 2:
+            if name[1].isdigit() and name[0] == 'H' and \
+                    self.resname in library._aacids:
+                name = name[1] + name[0] + '  '
             else:
-                name = ' '+name+ ' '
-        elif len(name)==1:
-            name = ' '+name+'  '
+                name = ' ' + name + ' '
+        elif len(name) == 1:
+            name = ' ' + name + '  '
         self.long_name = name
 
     def copy(self):
@@ -306,22 +310,31 @@ class Atom:
             # unknown residue
             c1 = self.long_name[1]
             c2 = self.long_name[2]
-            if c1=='C':
+            if c1 == 'C':
                 self.symbol = 'C'
-                if c2.upper()=='L': self.symbol = 'CL'
-                #elif c2.upper()=='A': self.symbol = 'CA'
-                #else:
+                if c2.upper() == 'L':
+                    self.symbol = 'CL'
+                # elif c2.upper()=='A': self.symbol = 'CA'
+                # else:
                 #    self.symbol = 'C'
-            elif c1 == 'O': self.symbol = 'O'
-            elif c1 == 'N': self.symbol = 'N'
-            elif c1 == 'H': self.symbol = 'H'
-            elif c1 == 'S': self.symbol = 'S'
-            elif c1 == 'P': self.symbol = 'P'
+            elif c1 == 'O':
+                self.symbol = 'O'
+            elif c1 == 'N':
+                self.symbol = 'N'
+            elif c1 == 'H':
+                self.symbol = 'H'
+            elif c1 == 'S':
+                self.symbol = 'S'
+            elif c1 == 'P':
+                self.symbol = 'P'
             elif c1 == 'B':
-                if c2.upper()=='R': self.symbol = 'BR'
+                if c2.upper() == 'R':
+                    self.symbol = 'BR'
             elif c1 == 'F':
-                if c2.upper()=='E': self.symbol = 'FE'
-                else: self.symbol = 'F'
+                if c2.upper() == 'E':
+                    self.symbol = 'FE'
+                else:
+                    self.symbol = 'F'
             elif c1 == 'I':
                 self.symbol = 'I'
             elif c1 == 'D':
@@ -330,8 +343,9 @@ class Atom:
                 self.symbol = 'D'
             else:
                 self.symbol = 'UN'
-##             print 'Using symbol %s for atom %s-%s' %\
-##                   (self.symbol,self.name,self.resname) 
+# print 'Using symbol %s for atom %s-%s' %\
+# (self.symbol,self.name,self.resname)
+
     def get_order(self):
         """ get the order (number of bonds to mainchain)"""
         if self.long_name == '':
@@ -341,33 +355,47 @@ class Atom:
         if self.resname not in library._protein_residues:
             print 'Sorry, implemented for proteins only'
             return
-        
+
         el = self.symbol
         x = self.long_name[2]
-        if self.name in ['C','CA','N']: self.order = 0
-        elif self.name in ['H','H1','H2','H3','1H','2H',
-                           '3H','O','O1',
-                           'O2','OC1','OC2','OXT',
-                           'OT1','OT2']:
+        if self.name in ['C', 'CA', 'N']:
+            self.order = 0
+        elif self.name in ['H', 'H1', 'H2', 'H3', '1H', '2H',
+                           '3H', 'O', 'O1',
+                           'O2', 'OC1', 'OC2', 'OXT',
+                           'OT1', 'OT2']:
             self.order = 1
-        else:    
-            if el!='H':
-                if x=='B': self.order = 1
-                elif x=='G': self.order = 2
-                elif x=='D': self.order = 3
-                elif x=='E': self.order = 4
-                elif x=='Z': self.order = 5
-                elif x=='H': self.order = 6
-            elif el=='H':
-                if x=='A': self.order = 1
-                elif x=='B': self.order = 2
-                elif x=='G': self.order = 3
-                elif x=='D': self.order = 4
-                elif x=='E': self.order = 5
-                elif x=='Z': self.order = 6
-                elif x=='H': self.order = 7
+        else:
+            if el != 'H':
+                if x == 'B':
+                    self.order = 1
+                elif x == 'G':
+                    self.order = 2
+                elif x == 'D':
+                    self.order = 3
+                elif x == 'E':
+                    self.order = 4
+                elif x == 'Z':
+                    self.order = 5
+                elif x == 'H':
+                    self.order = 6
+            elif el == 'H':
+                if x == 'A':
+                    self.order = 1
+                elif x == 'B':
+                    self.order = 2
+                elif x == 'G':
+                    self.order = 3
+                elif x == 'D':
+                    self.order = 4
+                elif x == 'E':
+                    self.order = 5
+                elif x == 'Z':
+                    self.order = 6
+                elif x == 'H':
+                    self.order = 7
 
-    def read_mol2_line(self,line):
+    def read_mol2_line(self, line):
         entr = line.split()
         if len(entr) == 9:
             self.id = int(entr[0])
@@ -398,6 +426,3 @@ class Atom:
             print line
             sys.exit(1)
         return self
-    
-
-
